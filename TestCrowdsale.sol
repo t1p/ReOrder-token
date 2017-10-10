03825d290c303b9b17941a0e18ba009839722920
igor <igor.klimovskiy@gmail.com>
2017-10-10 22:48:00 +0300

    Тестовый контракт для личного кабинета.

---
+++ TestCrowdsale.sol
@@ -0,0 +1,210 @@
+pragma solidity ^0.4.13;
+
+
+contract ERC20Basic {
+    uint256 public totalSupply;
+
+    function balanceOf(address who) constant public returns (uint256);
+
+    function transfer(address to, uint256 value) public returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) constant public returns (uint256);
+
+    function transferFrom(address from, address to, uint256 value) public returns (bool);
+
+    function approve(address spender, uint256 value) public returns (bool);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+library SafeMath {
+
+    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+
+}
+
+
+contract BasicToken is ERC20Basic {
+
+    using SafeMath for uint256;
+
+    mapping (address => uint256) balances;
+
+    // Fix for the ERC20 short address attack
+    modifier onlyPayloadSize(uint size) {
+        require(msg.data.length >= size + 4);
+        _;
+    }
+
+    function transfer(address _to, uint256 _value) onlyPayloadSize(2 * 32) public returns (bool) {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) constant public returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+}
+
+
+contract StandardToken is ERC20, BasicToken {
+
+    mapping (address => mapping (address => uint256)) allowed;
+
+    function transferFrom(address _from, address _to, uint256 _value) onlyPayloadSize(3 * 32) public returns (bool) {
+        var _allowance = allowed[_from][msg.sender];
+
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) onlyPayloadSize(2 * 32) public returns (bool) {
+
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) onlyPayloadSize(2 * 32) constant public returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+}
+
+
+contract owned {
+
+    address public owner;
+
+    address public newOwner;
+
+    function owned() public payable {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(owner == msg.sender);
+        _;
+    }
+
+    function changeOwner(address _owner) onlyOwner public {
+        require(_owner != 0);
+        newOwner = _owner;
+    }
+
+    function confirmOwner() public {
+        require(newOwner == msg.sender);
+        owner = newOwner;
+        delete newOwner;
+    }
+}
+
+
+contract MintableToken is StandardToken, owned {
+
+    event Mint(address indexed to, uint256 amount);
+
+    event MintFinished();
+
+    bool public mintingFinished = false;
+
+    modifier canMint() {
+        require(!mintingFinished);
+        _;
+    }
+
+    function mint(address _to, uint256 _amount) onlyOwner canMint onlyPayloadSize(2 * 32) public returns (bool) {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        Mint(_to, _amount);
+        return true;
+    }
+
+    function finishMinting() onlyOwner public returns (bool) {
+        mintingFinished = true;
+        MintFinished();
+        return true;
+    }
+
+}
+
+
+contract TestingCoin is MintableToken {
+
+    string public constant name = "Testing Coin";
+
+    string public constant symbol = "TC";
+
+    uint32 public constant decimals = 15;
+}
+
+
+/**
+ * @title Crowdsale
+ * @dev Implementation for 3 phases crowdsale.
+*/
+contract Crowdsale is owned {
+
+    using SafeMath for uint256;
+
+    TestingCoin token;
+
+    uint256 public totalSupply = 0;
+    uint256 public totalAmount = 0;
+    uint256 public currentAmount = 0;
+
+    function Crowdsale() public {
+        token = new TestingCoin();
+    }
+
+    function() external payable {
+        uint256 amount = msg.value;
+
+        totalAmount = totalAmount.add(amount);
+        currentAmount = currentAmount.add(amount);
+
+        totalSupply = totalSupply.add(amount);
+        token.mint(msg.sender, amount);
+    }
+
+    function withdraw() external onlyOwner {
+        require(msg.sender.call.gas(3000000).value(currentAmount)());
+        currentAmount = 0;
+    }
+}
